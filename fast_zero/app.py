da1794564fb5930ddb485d81041d74b4a0eb124f
baronetogio <giovcb@hotmail.com>
2024-07-19 19:36:05 -0300

    Implement CRUD routes

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,59 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+db = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': db}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(db) + 1, **user.model_dump())
+    db.append(user_with_id)
+    return user_with_id
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def read_user(user_id: int):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = db[user_id - 1]
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    db[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_users(user_id: int):
+    if user_id < 1 or user_id > len(db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    del db[user_id - 1]
+    return {'message': 'User deleted'}
